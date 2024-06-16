76b3ccf5df4c382753c812209cc69f1e39bf1611
SofiiaNykon <sofianikonovic@gmail.com>
2024-06-16 20:54:38 +0200

    + creating element

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-async function userFetch(){
-  const result  = await fetch('https://fakestoreapi.com/products/?_limit=12');
-
-    return result.json();
+const userFetch = async () =>{
+  const response  = await fetch('https://fakestoreapi.com/products?limit=12');
+   const productFetch =  await response.json();
+    return productFetch;
 }                 //creating element
               async function createContainer(){
                 const products = await userFetch();
@@ -12,30 +12,42 @@ async function userFetch(){
                 const img = document.createElement("img");
                 const productTitle = document.createElement("h1");
                 const productDescribe = document.createElement("p");
-                const productItemPrice = document.createElement("div");
                 const price = document.createElement("span");
                 const button = document.createElement("button");
                 //adding classes
-                productItem.classList.add(".product-item");
-                productTitle.classList.add(".product-item h1");
-                productDescribe.classList.add(".product-item p");
-                productItemPrice.classList.add(".product-item-price");
-                price.classList.add(".product-item-price span");
-                button.classList.add(".product-price-button");
-                
-              
-                img.innerHTML = product.image;
+                productItem.classList.add("product-item");
+                img.classList.add("pictures");
+                productTitle.classList.add("title");
+                productDescribe.classList.add("product-description");
+                price.classList.add("product-item-price-span");
+                button.classList.add("product-price-button");
+            
+                img.src = product.image;
+                console.log(img)
                 productTitle.innerHTML = product.title;
                 productDescribe.innerHTML = product.description;
                 price.innerHTML = product.price;
                 button.innerText = "Add";
               
-                productItem.append(img, productTitle, productDescribe);
-                productItemPrice.append(price, button);
-                list.append(productItem, productItemPrice);
+                productItem.append(img, productTitle, productDescribe, price, button);
+                list.append(productItem);
               }
 }
 createContainer()
+
+button.onclick(){
+const cart = document.querySelector(".cart");
+const cartEmptyTitle = document.querySelector(".cart-list-titles");
+const cartListWrapper = document.querySelector(".cart-list-wrapper");
+const cartListTitles = document.querySelector(".cart-list-titles");
+const cartListItemTitle = document.querySelector(".cart-list-item-title");
+const  cartListQuantityTitle = document.querySelector(".cart-list-quantity-title");
+const cartList = document.querySelector(".cart-list");
+const totalAmount = document.querySelector(".total-amount");
+
+
+
+}
 
 // button.onclick (){
 // const counter = 0;
